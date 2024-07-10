c50839451b191e58b2e3f631fb26f0388b10c38d
mamatsalay <mamatsalaybabadjanov@gmail.com>
2024-07-10 18:13:53 -0500

    1st

---
+++ src/main/java/uz/incomeservicemanager/IncomeServiceManagerApplication.java
@@ -0,0 +1,13 @@
+package uz.incomeservicemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncomeServiceManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IncomeServiceManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/incomeservicemanager/IncomeServiceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.incomeservicemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncomeServiceManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
