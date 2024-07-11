389522cf97e3e2ae54b836700f8b78e7e2058470
mamatsalay <mamatsalaybabadjanov@gmail.com>
2024-07-10 20:52:41 -0500

    income controller class

---
+++ src/main/java/uz/incomeservicemanager/controller/IncomeController.java
@@ -0,0 +1,49 @@
+package uz.incomeservicemanager.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uz.incomeservicemanager.model.Income;
+import uz.incomeservicemanager.service.IncomeService;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/incomes")
+public class IncomeController {
+
+    @Autowired
+    private IncomeService incomeService;
+
+    @GetMapping
+    public ResponseEntity<Iterable<Income>> getAllIncomes() {
+        Iterable<Income> incomes = incomeService.getAllIncome();
+        return ResponseEntity.ok(incomes);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Income> getIncome(@PathVariable Long id) {
+        Income income = incomeService.getIncome(id);
+        return ResponseEntity.ok(income);
+    }
+
+    @PostMapping
+    public ResponseEntity<Income> createIncome(@RequestBody Income income) {
+        Income savedIncome = incomeService.saveIncome(income);
+        return ResponseEntity.ok(savedIncome);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Income> updateIncome(@PathVariable Long id, @RequestBody Income income) {
+        income.setId(id);
+        Income updatedIncome = incomeService.saveIncome(income);
+        return ResponseEntity.ok(updatedIncome);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteIncome(@PathVariable Long id) {
+        incomeService.deleteIncome(id);
+        return ResponseEntity.noContent().build();
+    }
+}
