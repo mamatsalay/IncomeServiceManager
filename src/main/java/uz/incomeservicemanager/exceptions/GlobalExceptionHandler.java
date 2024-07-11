cb152d0bd4717cabf4669e2a1080233392c99415
mamatsalay <mamatsalaybabadjanov@gmail.com>
2024-07-10 20:52:07 -0500

    exception handler

---
+++ src/main/java/uz/incomeservicemanager/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package uz.incomeservicemanager.exceptions;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IncomeNotFoundException.class)
+    public ResponseEntity<?> handleIncomeNotFoundException(IncomeNotFoundException ex, WebRequest request) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleGlobalException(Exception ex, WebRequest request) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
